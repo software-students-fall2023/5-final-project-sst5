4b130a8627a13d8fe32dae1450e292acbed0343d
tinybitofheaven <abc9295@nyu.edu>
2023-12-16 17:16:50 -0500

    update table and some minor functionality

--- web_app/static/game.js
+++ web_app/static/game.js
@@ -1,62 +1,64 @@
 let guesses = 0;
 
 document.addEventListener("DOMContentLoaded", function () {
-  var guessForm = document.getElementById("guessForm");
+	var guessForm = document.getElementById("guessForm");
 
-  var answerElement = document.getElementById("answer");
-  var answer = answerElement.dataset.answer;
+	var answerElement = document.getElementById("answer");
+	var answer = answerElement.dataset.answer;
 
-  guessForm.addEventListener("submit", function (event) {
-    event.preventDefault();
+	guessForm.addEventListener("submit", function (event) {
+		event.preventDefault();
 
-    var pokemonGuess = $('#guessPokemon').val();
-    let correctness;
+		var pokemonGuess = $("#guessPokemon").val();
+		let correctness;
 
-    fetch("compare", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ name: pokemonGuess, answer: answer }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-          $('#feedback-text').text(data['msg']);
-          if(data['Pokemon']){
-              guesses++;
-              correctness = getCorrectness(data);
-              comparisons.addGuess(data, correctness);
-              if(checkWin(correctness)){
-                  $('#feedback-text').text("You guessed it!");
-                  updateLeaderboard(answer, guesses);
-                  $('.win').show();
-              }
-              $('#guessPokemon').val('');
-          }
-          console.log(data);
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-      });
-  });
+		fetch("compare", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({ name: pokemonGuess, answer: answer }),
+		})
+			.then((response) => response.json())
+			.then((data) => {
+				$("#feedback-text").text(data["msg"]);
+				if (data["Pokemon"]) {
+					guesses++;
+					$("#guessNumber").text("Number of Guesses: " + guesses);
+					correctness = getCorrectness(data);
+					comparisons.addGuess(data, correctness);
+					if (checkWin(correctness)) {
+						$("#feedback-text").text("You guessed it!");
+						document.querySelector("#guessPokemonBtn").setAttribute("disabled", "");
+						updateLeaderboard(answer, guesses);
+						$(".win").show();
+					}
+					$("#guessPokemon").val("");
+				}
+				console.log(data);
+			})
+			.catch((error) => {
+				console.error("Error:", error);
+			});
+	});
 });
 
 function updateLeaderboard(pokemonName, guesses) {
-  fetch("/update_leaderboard", {
-      method: "POST",
-      headers: {
-          "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-          pokemon: pokemonName,
-          guesses: guesses
-      }),
-  })
-  .then((response) => response.json())
-  .then((data) => {
-      console.log("Leaderboard updated:", data);
-  })
-  .catch((error) => {
-      console.error("Error updating leaderboard:", error);
-  });
+	fetch("/update_leaderboard", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({
+			pokemon: pokemonName,
+			guesses: guesses,
+		}),
+	})
+		.then((response) => response.json())
+		.then((data) => {
+			console.log("Leaderboard updated:", data);
+		})
+		.catch((error) => {
+			console.error("Error updating leaderboard:", error);
+		});
 }
